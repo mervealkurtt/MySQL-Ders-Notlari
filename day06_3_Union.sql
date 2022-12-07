7e24cba02a92cd05d800bcfd0e26a93a4875b3de
Merve Alkurt <mrvalkrtt97@gmail.com>
2022-12-07 08:35:58 +0300

    day06_3_union

--- day06_3_Union.sql
+++ day06_3_Union.sql
@@ -50,8 +50,9 @@ SELECT * FROM personel;
 -- select isim, maas from personel where maas >= 4000;
 -- select sehir, maas from personel where maas >= 5000;
 
-select isim, maas from personel where maas >= 4000 union
+select isim as isim_sehir, maas from personel where maas >= 4000 union
 select sehir, maas from personel where maas >= 5000;
+
 
 
 /* -----------------------------------------------------------------------------
@@ -59,19 +60,34 @@ select sehir, maas from personel where maas >= 5000;
   personelin maaslarini yüksekten alçaga dogru siralayarak bir tabloda gosteren 
   sorguyu yaziniz.    
 ------------------------------------------------------------------------------*/  
+-- select isim, maas from personel where isim='Mehmet Ozturk';
+-- select sehir, maas from personel where sehir='Istanbul' order by maas desc;
+
+select isim as isim_sehir, maas from personel where isim='Mehmet Ozturk' union
+select sehir, maas from personel where sehir='Istanbul' order by maas desc;
 
 
 
 /* -----------------------------------------------------------------------------
   SORU3: Honda,Ford ve Tofas’ta calisan  personelin ismini listeleyin
 ------------------------------------------------------------------------------*/  
+select isim, sirket from personel where sirket ='Honda' union
+select isim, sirket from personel where sirket ='Ford' union
+select isim, sirket from personel where sirket ='Tofas';
+
+select isim, sirket from personel where sirket in ('Honda', 'Ford', 'Tofas');
 
 
 
 /* -----------------------------------------------------------------------------
-  SORU4: 5000’den az maas alanlarin bilgileri ile, arti Honda calisani olmayanlarin bilgilerini
+  SORU4: 5000’den az maas alanlarin bilgileri ile, Honda calisani olmayanlarin bilgilerini
  alt alta listeleyen bir sorgu yaziniz. 
 ------------------------------------------------------------------------------*/ 
+-- select * from personel where maas <5000;
+-- select * from personel where sirket <> 'Honda';
+
+select * from personel where maas <5000 union
+select * from personel where sirket <> 'Honda';
 
 
 
@@ -79,9 +95,51 @@ select sehir, maas from personel where maas >= 5000;
   SORU5: Ismi Mehmet Ozturk olanlarin sehirlerini + Istanbul’da calismayanlarin isimlerini ve 
   sehirlerini listeleyen sorguyu yaziniz.
 ------------------------------------------------------------------------------*/
+-- select isim, sehir from personel where isim = 'Mehmet Ozturk';
+-- select isim, sehir from personel where sehir != 'Istanbul';
+
+select isim, sehir from personel where isim = 'Mehmet Ozturk' union 
+select isim, sehir from personel where sehir != 'Istanbul';
 
 
 
+
+
+/*======================== FARKLI TABLOLARDAN BIRLESTIRME ====================*/   
+    
+CREATE TABLE personel_bilgi 
+(
+	id int, 
+	tel char(10) UNIQUE , 
+	cocuk_sayisi int
+); 
+   
+INSERT INTO personel_bilgi VALUES(123, '5302345678' , 5);
+INSERT INTO personel_bilgi VALUES(234, '5422345678', 4);
+INSERT INTO personel_bilgi VALUES(345, '5354561245', 3); 
+INSERT INTO personel_bilgi VALUES(478, '5411452659', 3);
+INSERT INTO personel_bilgi VALUES(789, '5551253698', 2);
+INSERT INTO personel_bilgi VALUES(344, '5524578574', 2);
+INSERT INTO personel_bilgi VALUES(125, '5537488585', 1);
+
+
+select * from personel_bilgi;
+
+
+/* -----------------------------------------------------------------------------
+  SORU6: id’si 123456789 olan personelin Personel tablosundan sehir ve 
+  maasini, personel_bilgi tablosundan da (id ilk 3 hanesiyle kaydolmuş=123)
+  tel ve cocuk sayisini yazdirin  
+------------------------------------------------------------------------------*/    
+select sehir, maas from personel where id = '123456789';
+select tel, cocuk_sayisi from personel_bilgi where id = '123';
+
+select sehir, maas from personel where id = '123456789'union
+select tel, cocuk_sayisi from personel_bilgi where id = '123';
+/* -----------------------------------------------------------------------------
+  SORU7: Personel tablosundan Istanbul veya Ankara’da calisanlarin id’lerini
+ ve Personel_bilgi tablosundan 2 veya 3 cocugu olanlarin id lerini sorgulayiniz.
+------------------------------------------------------------------------------*/
 
 
 
